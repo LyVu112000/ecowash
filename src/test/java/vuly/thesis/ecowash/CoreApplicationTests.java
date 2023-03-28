8b43e6a6ba1e2cc33acc5b151029a3dface56109
Lyhgvu <lyhgvu@gmail.com>
2023-03-28 10:40:38 +0700

    Initial commit

---
+++ src/test/java/vuly/thesis/ecowash/CoreApplicationTests.java
@@ -0,0 +1,9 @@
+package vuly.thesis.ecowash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+}
